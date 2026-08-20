4d47046f968b5f2c82c80f4d28e793787fd5812a
Chiranjiv Kushwah <121075484+chiranjiv09@users.noreply.github.com>
2026-08-20 23:32:43 +0530

    Add: binary-subarrays-with-sum - submission-0

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+
+            //          atMost(goal)
+            //   ┌─────────────────┐
+            //   │ sum < goal      │
+            //   │ sum == goal  ←──┼── We want this
+            //   └─────────────────┘
+            //            -
+            //      atMost(goal-1)
+            //   ┌─────────────────┐
+            //   │ sum < goal      │
+            //   └─────────────────┘
+            //            =
+            //      sum == goal
+    public int  getSmallerEqualsPairs(int[] nums, int goal) {
+        if(goal < 0) return 0;
+        int l=0;
+        int sum = 0;
+        int count = 0;
+
+        for(int r=0; r<nums.length; r++) {
+            sum += nums[r];
+
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
+            }
+            count += (r-l+1);
+        }
+        return count;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return getSmallerEqualsPairs(nums, goal) - getSmallerEqualsPairs(nums, goal-1);
+    }
+}
